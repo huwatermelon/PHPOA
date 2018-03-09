3455716153465b82691063e172d7a8a18b8aa0ee
wangwust <1277955953@qq.com>
2018-03-09 18:37:35 +0800

    首次提交

---
+++ webmain/home/desktop/footer.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+*	桌面首页项(底部版权提示)
+*/
+defined('HOST') or die ('not access');
+
+?>
+
+<div class="tishi" align="center">Copyright &copy;<?=date('Y')?>  &nbsp;<?=substr(URLY,7,-1)?> &nbsp;<?=TITLE?>v<?=VERSION?> </div>
